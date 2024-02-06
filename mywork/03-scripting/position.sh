9504ddd8c321c9a9a378b720165e5358da534a56
Tiffany Bui <tiffanybui@Tiffanys-MacBook-Pro.local>
2024-02-06 16:44:09 -0500

    class feb 6 24

---
+++ mywork/03-scripting/position.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# echo "$0 <-- is the 0 position"
+# echo "$1 <-- is the 1 position"
+# echo "$2 <-- is the 2 position"
+# echo "$3 <-- is the 3 position"
+
+echo "My name is $1"
---
+++ mywork/03-scripting/readme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script is designed to __
+# Author: Tiffany Bui <tnb6zdz@virginia.edu>
+# Last updated: 2/6/24
+
+read -p "What is your favorite number?" FAVNUM
+echo "Hey $USER I heard your favorite number is $FAVNUM"
