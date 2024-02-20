ad554374c4341488358f61f21d3570a307662036
Tiffany Bui <tiffanybui@Tiffanys-MBP.Home>
2024-02-20 01:42:49 -0500

    lab3 madlib

---
+++ mywork/lab3/madlib.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me an adjective: " ADJ2
+read -p "3. Please give me a part of the body: " BODY1
+read -p "4. Please give me a noun: " NOUN1 
+read -p "5. Please give me an animal: " ANIMAL1
+read -p "6. Please give me a verb: " VERB1 
+read -p "7. Please give me an adverb: " ADVERB1
+read -p "8. Please give me a noun: " NOUN2
+read -p "9. Please give me an adjective: " ADJ3  
+read -p "10. Please give me a noun: " NOUN3 
+read -p "11. Please give me a verb: " VERB2
+read -p "12. Please give me a noun: " NOUN4
+read -p "13. Please give me a part of the body: " BODY2
+read -p "14. Please give me a noun: " NOUN5
+read -p "15. Please give me an adverb: " ADVERB2
+read -p "16. Please give me a verb: " VERB3
+read -p "17. Please give me a verb: " VERB4
+read -p "18. Please give me an exclamation: " EXCLAMATION1
+read -p "19. Please give me a past tense verb: " PASTVERB1  
+read -p "20. Please give me a past tense verb: " PASTVERB2 
+read -p "21. Please give me a number: " NUMBER1 
+read -p "22. Please give me a number: " NUMBER2
+read -p "23. Please give me a verb: " VERB5 
+read -p "24. Please give me a verb: " VERB6 
+read -p "25. Please give me a verb: " VERB7 
+read -p "26. Please give me an adverb: " ADVERB3 
+
+clear
+echo "When we first met, you looked $ADJ1 and $ADJ2."
+echo "I couldn't believe my $BODY1!"
+echo "You smelled like $NOUN1 and walked as graceful as a $ANIMAL1."
+echo -e "I couldn't wait to $VERB1 to you.\n"
+echo "I $ADVERB1 asked you if I could have your $NOUN2."
+echo "I could see a look of $ADJ3 in your eyes."
+echo -e "You hesitated for a moment, then gave me a $NOUN3.\n"
+echo "I finally got to take you on our first date. I loved spending time with you."
+echo "You could make me $VERB2 so easily, like no one had before."
+echo "when you touched me, I felt $NOUN4 up my $BODY2."
+echo -e "When you looked at me, I could see $NOUN5. I knew we were meant to be together.\n"
+echo "After a $ADVERB2 night, I was so $VERB3 to bring you home."
+echo "I longed to $VERB4 you again."
+echo "You looked at me and said, '$EXCLAMATION1' and I just knew."
+echo -e "You $PASTVERB1 me and I $PASTVERB2 you.\n"
+echo "Here we are after all this time. It feels like it's been $NUMBER1 years!"
+echo "I look forward to $NUMBER2 more."
+echo "You make me want to $VERB5 and I will $VERB6 you forever."
+echo -e "Nobody else could $VERB7 me, like you do.\n"
+echo "$ADVERB3 yours,"
+echo "Me"
+
+# i thought this was a cute mad lib I found on pinterest: https://oneshetwoshe.com/mad-libs-love-story/
+# but now i read it and it's kind of odd, sorry about that
